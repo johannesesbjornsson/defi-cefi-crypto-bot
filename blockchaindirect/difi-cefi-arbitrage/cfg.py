96a8b43be2b532a15e5865a5f3ee18c337cb5a99
Johannes Esbjörnsson <johannesesbjornsson@Johanness-MBP.fritz.box>
2021-12-17 13:31:55 +0000

    Improving

---
+++ blockchaindirect/difi-cefi-arbitrage/cfg.py
@@ -0,0 +1,8 @@
+import os
+
+
+bsc_scan_api_key = os.environ['bsc_scan_api_key']
+my_bep20_address=os.environ['my_bep20_address']
+private_key=os.environ['private_key']
+api_key = os.environ['binance_api']
+api_secret = os.environ['binance_secret']
---
+++ blockchaindirect/pancakeswap-arbitrage/cfg.py
@@ -0,0 +1,6 @@
+import os
+
+
+bsc_scan_api_key = os.environ['bsc_scan_api_key']
+my_bep20_address=os.environ['my_bep20_address']
+private_key=os.environ['private_key']
