51a5552f34af0fb11f3db8a7906d46b2583b6886
Johannes Esbjörnsson <johannesesbjornsson@Johanness-MBP.fritz.box>
2021-12-18 15:08:05 +0000

    Improving

---
+++ blockchaindirect/polygon-arbitrage/cfg.py
@@ -0,0 +1,5 @@
+import os
+
+
+my_polygon_address=os.environ['my_bep20_address']
+private_key=os.environ['private_key']
---
+++ blockchaindirect/polygon-arbitrage/decode_wei.py
@@ -0,0 +1,15 @@
+from web3 import Web3
+
+bsc = "https://bsc-dataseed.binance.org/"
+#bsc = "https://data-seed-prebsc-1-s1.binance.org:8545"
+web3 = Web3(Web3.HTTPProvider(bsc))
+price =  142530937387683739 
+price =   1
+amount = web3.fromWei(price,'ether')
+amount = web3.toWei(price,'ether')
+print(amount)
+
+# Hexstrings https://web3py.readthedocs.io/en/stable/web3.main.html
+
+#1 -> 1000000000000000000
+print(Web3.toBytes(0))
