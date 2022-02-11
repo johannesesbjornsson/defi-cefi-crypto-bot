093958d3c05c6dd24ff33ec306dfcae34bd3066a
Johannes Esbjörnsson <johannesesbjornsson@Johanness-MBP.fritz.box>
2022-02-11 18:03:01 +0000

    fine-tuning-slippage-and-txn-value

--- WIP/v1-test/libraries/eth_fork_token.py
+++ WIP/v1-test/libraries/eth_fork_token.py
@@ -4,12 +4,12 @@ import asyncio
 from eth_fork_transaction import Transaction
 
 import asyncio
-import nest_asyncio
-nest_asyncio.apply()
+#import nest_asyncio
+#nest_asyncio.apply()
 
 class Token(object):
 
-    def __init__(self, client, token, init_type="standard"):
+    def __init__(self, client, token, init_type="standard",):
         self.known_tokens = client.known_tokens
         self.client = client
 
@@ -20,23 +20,38 @@ class Token(object):
             self.address = self.client.web3.toChecksumAddress(self.known_tokens[token])
             self.name = token
 
-        
         self.token_contract = self.client.web3.eth.contract(
             address=self.address, 
             abi=contract_libarary.standard_contracts["token"])
-        
+
         if init_type == "standard":
             self.decimals = self.token_contract.functions.decimals().call()
+            self.verified = self.is_token_verified()
+            self.safe_code = self.has_safe_code()
         elif init_type == "local":
             token_info = self.client.get_token_info(self.address)
             if token_info:
                 self.decimals = token_info["decimals"]
+                self.verified = token_info["verified"]
+                self.safe_code = token_info["safe_code"]
             else:
+                self.verified = self.is_token_verified()
+                self.safe_code = self.has_safe_code()
                 self.decimals = self.token_contract.functions.decimals().call()
-                token_info = { "decimals" : self.decimals }
+                token_info = { "decimals" : self.decimals, "verified" : self.verified, "safe_code": self.safe_code}
                 self.client.add_token_info(self.address, token_info)
+        elif init_type == "live":
+            token_info = self.client.get_token_info(self.address)
+            if token_info:
+                self.decimals = token_info["decimals"]
+                self.verified = token_info["verified"]
+                self.safe_code = token_info["safe_code"]
+            else:
+                self.decimals = None
+                self.verified = False
+                self.safe_code = False
         else:
-            raise ValueError("'init_type' needs to be 'standard' or 'local'")
+            raise ValueError("'init_type' needs to be 'standard', 'live' or 'local'")
 
         self.token_symbol = None
         self.allowance = None
@@ -55,6 +70,37 @@ class Token(object):
         if not self.allowance:
             self.allowance = self.token_contract.functions.allowance(self.client.my_address,self.client.router_contract_address).call()
         return self.allowance
+
+    def has_safe_code(self):
+        safe_code = True
+        dodgy_code_statements = [
+            "function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool)",
+            "marketingFee",
+            #"function _approve(address owner, address spender, uint256 amount) internal",
+            #"mint"
+        ]
+        response_code, response_json = self.client.get_abi(self.address)
+        if response_code == 200:
+            if "SourceCode" in response_json["result"][0]:
+                #for line in response_json["result"][0]["SourceCode"].split("\n"):
+                #    print(line)
+                for code_statement in dodgy_code_statements:
+                    if code_statement in response_json["result"][0]["SourceCode"]:
+                        safe_code = False
+        else:
+            raise LookupError("Not 200 reponse")
+        return safe_code
+
+    def is_token_verified(self):
+        verified = False
+        response_code, response_json = self.client.get_abi(self.address)
+        if response_code == 200:
+            if "ABI" in response_json["result"][0]:
+                if response_json["result"][0]["ABI"] != "Contract source code not verified":
+                    verified = True
+        else:
+            raise LookupError("Not 200 reponse")
+        return verified
 
     def set_proxy_details(self):
         is_proxy = False
@@ -78,17 +124,7 @@ class Token(object):
             txn = self.token_contract.functions.approve(self.client.router_contract_address,value)
             transaction = Transaction(self.client, None)
             transaction.create_transaction(txn)
-            try:
-                transaction.sign_and_send_transaction()
-            except ValueError as e:
-                print("str stard:",str(e),":str end")
-                print(type(e))
-                if str(e) == "{'code': -32000, 'message': 'nonce too low'}":
-                    print("Having to resend transaction")
-                    transaction.nonce += 1
-                    transaction.sign_and_send_transaction()
-                else:
-                    raise ValueError(str(e))
+            transaction.sign_and_send_transaction()
 
             transaction_complete, transaction_successful = transaction.get_transaction_receipt(wait=True)
             if not transaction_successful:
